0e5ad165b4d293504717e97e8634d8f83a7b8949
Stdio <kwork-stdio@mail.ru>
2019-10-22 22:49:38 +0300

    0.0.1

--- app/src/main/java/com/stdio/medicalabbreviationsdictionary/MainActivity.java
+++ app/src/main/java/com/stdio/medicalabbreviationsdictionary/MainActivity.java
@@ -1,14 +1,42 @@
 package com.stdio.medicalabbreviationsdictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<DataModel> dataList;
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        rv=(RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        initializeData();
+        initializeAdapter();
+    }
+
+    private void initializeData(){
+        dataList = new ArrayList<>();
+        dataList.add(new DataModel("Wert", "Показатель, величина"));
+        dataList.add(new DataModel("Ergebnis", "результат"));
+        dataList.add(new DataModel("Kommentar", "комментарий"));
+    }
+
+    private void initializeAdapter(){
+        RVAdapter adapter = new RVAdapter(dataList, this);
+        rv.setAdapter(adapter);
     }
 }
